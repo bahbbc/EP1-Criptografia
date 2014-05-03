a665748c7b0a634a52441f206a6f5ecd94331a4c
Barbara Barbosa <barbarab.claudono@gmail.com>
2014-05-02 23:22:25 -0300

    Adicao do hill ao main

--- mainExemplo.m
+++ mainExemplo.m
@@ -5,12 +5,12 @@ C = monoalfabeticaE(P,'knmporqtsvuxwzybadcfeghjil');
 [rateMono character] = countCharacters(C);
 C2 = vigenere(P,'knmporqtsvuxwzybadcfeghjil');
 [rateMono2 character] = countCharacters(C2);
-%C3 = vigenereIncrementado(P,'rdf');
-%[rateMono3 character] = countCharacters(C3);
+C3 = hill(P, [17, 17, 5; 21,18,21;2,2,19]);
+[rateMono3 character] = countCharacters(C3);
 
     
-plot([1:26],[rateMono;rateMono2]);
-legend('Monoalfabetica', 'Vigenere');
+plot([1:26],[rateMono;rateMono2, rateMono3]);
+legend('Monoalfabetica', 'Vigenere', 'Hill');
 title('Frequencia de Letras em Textos Cifrados');
 xlabel('letras');
 ylabel('percentagem de ocorrencia');
